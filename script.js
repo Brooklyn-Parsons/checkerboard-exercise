587c840bc62a3f5aadbe100ff446d0721e3d72b4
Brooklyn <brooklyndparsons@gmail.com>
2023-04-05 15:15:02 -0500

    redblack

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Your JS goes here
+const container = document.createElement("div");
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = "800px";
+container.style.height = "800px";
+document.body.append(container);
+
+for (let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j ++){
+    const tile = document.createElement("div");
+    tile.style.width = "12.5%"
+    tile.style.height = "12.5%"
+    if ((i + j) % 2 === 0){
+    tile.style.background = "red"; 
+    } else {
+        tile.style.background = "black";
+    }
+    container.append(tile);
+    }
+}
+
+
+
